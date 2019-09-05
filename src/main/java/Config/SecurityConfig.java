586cc5f71d078e300a3aa61068bd3fc1cf2a9b36
ihsanizwer <ihsanizwer@yahoo.com>
2019-09-05 16:19:47 +0530

    Addded LogOutSuccessURL

--- src/main/java/Config/SecurityConfig.java
+++ src/main/java/Config/SecurityConfig.java
@@ -62,6 +62,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .logout()
                 .logoutUrl("/signing_out")
+                .logoutSuccessUrl("sigin.jsp")
                 .deleteCookies("JSESSIONID")
                 .logoutSuccessHandler(lSH);
     }
